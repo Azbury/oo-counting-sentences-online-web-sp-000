0c09dcb6c9cf225fa1b104fdd590169f79249bc2
Austin Asbury <acasbury25@aol.com>
2019-08-26 14:36:22 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class String
 
   def sentence?
@@ -16,11 +14,7 @@ class String
 
   def count_sentences
     sentences = self.split(/\.|\?|\!/)
-<<<<<<< HEAD
     sentences.delete_if {|s| s.size < 2}
-=======
-    puts sentences
->>>>>>> 7c5ff07273626d2fa72611efd26aedd6bc1622ba
     sentences.size
   end
 end
